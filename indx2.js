14e756741ca66efbbe7af6ce7c140e17e2cdf677
zohrehchenari <zohrehchenari2874@gmail.com>
2023-10-08 18:57:04 +0330

    add all files

---
+++ indx2.js
@@ -0,0 +1,6 @@
+ const toggler=document.querySelector(".nav__toggler");
+ const navbar=document.querySelector(".nav");
+ toggler.addEventListener("click" ,(e)=>{
+    console.log("clicked");
+    navbar.classList.toggle("expanded");
+ })
